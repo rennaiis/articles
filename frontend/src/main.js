f850999a574c4bfd69fc4c72e3125920a7224a31
a.isaeva <201250056+rennaiis@users.noreply.github.com>
2025-10-05 17:48:40 +0300

    vuetify установлен и подключен к проекту

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify()
+
+import router from './router'
+import store from './store'
+
+createApp(App).use(store).use(router).mount('#app').use(vuetify)
