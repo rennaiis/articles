638a073dfbea9372464c5ff7d48eb3c476eac569
a.isaeva <201250056+rennaiis@users.noreply.github.com>
2025-10-05 21:59:46 +0300

    Добавление в articlesList компонента "data iterator" из vuetify

---
+++ frontend/src/pages/ArticlesList.vue
@@ -0,0 +1,21 @@
+<v-data-iterator :items="items" page="page">
+  <template v-slot:default="{ items }">
+    <template
+      v-for="(item, i) in items"
+      :key="i"
+    >
+      <v-card v-bind="item.raw"></v-card>
+
+      <br>
+    </template>
+  </template>
+</v-data-iterator>
+<script setup>
+  import { ref } from 'vue'
+
+  const page = ref(1)
+  const items = Array.from({ length: 15 }, (k, v) => ({
+    title: 'Item ' + v + 1,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!',
+  }))
+</script>
