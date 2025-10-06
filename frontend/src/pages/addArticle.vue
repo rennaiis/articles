ede24df30fd263d2f8e1c46dced036e0bb54dd86
a.isaeva <201250056+rennaiis@users.noreply.github.com>
2025-10-06 14:50:35 +0300

    Создана страница для добавления статьи

--- frontend/src/pages/addArticle.vue
+++ frontend/src/pages/addArticle.vue
@@ -1,5 +1,19 @@
 <template>
-</template>
+  <v-sheet class="mx-auto" width="1000">
+    <v-form fast-fail @submit.prevent>
+      <v-text-field
+        v-model="firstName"
+        label="Заголовок статьи"
+      ></v-text-field>
 
+      <v-textarea
+        v-model="lastName"
+        label="текст статьи"
+      ></v-textarea>
+      <v-btn class="mt-2" type="submit" block>Создать</v-btn>
+    </v-form>
+  </v-sheet>
+</template>
 <script>
+
 </script>
