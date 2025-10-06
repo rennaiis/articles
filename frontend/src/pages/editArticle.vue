f77eb5e53d55896bc6c67e61348132f26f9be7ae
a.isaeva <201250056+rennaiis@users.noreply.github.com>
2025-10-06 15:40:26 +0300

    Страницы для редактирования статей и комментариев

--- frontend/src/pages/editArticle.vue
+++ frontend/src/pages/editArticle.vue
@@ -1,5 +1,24 @@
 <template>
+<v-container>
+  <v-sheet class="mx-auto">
+    <v-form fast-fail @submit.prevent>
+      <v-text-field
+        v-model="title"
+        :rules="titleRules"
+        label="Заголовок статьи"
+      ></v-text-field>
+
+      <v-textarea
+        v-model="content"
+        :rules="contentRules"
+        label="текст статьи"
+      ></v-textarea>
+      <v-btn class="mt-2" @click="onSubmit">Сохранить</v-btn>
+    </v-form>
+  </v-sheet>
+</v-container>
 </template>
+
 
 <script>
 </script>
--- frontend/src/pages/editComment.vue
+++ frontend/src/pages/editComment.vue
@@ -1,5 +1,18 @@
 <template>
+<v-container>
+  <v-sheet class="mx-auto">
+    <v-form fast-fail @submit.prevent>
+      <v-textarea
+        v-model="content"
+        :rules="contentRules"
+        label="Текст комментария"
+      ></v-textarea>
+      <v-btn class="mt-2" @click="onSubmit">Сохранить</v-btn>
+    </v-form>
+  </v-sheet>
+</v-container>
 </template>
+
 
 <script>
 </script>
