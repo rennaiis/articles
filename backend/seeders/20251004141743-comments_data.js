0aeb4c63cf957bfef74a19704de0067368cc87f7
a.isaeva <201250056+rennaiis@users.noreply.github.com>
2025-10-06 23:05:08 +0300

    Заполнение сидов тестовыми статьями и комментариями

--- backend/seeders/20251004141743-comments_data.js
+++ backend/seeders/20251004141743-comments_data.js
@@ -4,9 +4,11 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert("Comments", [
-      {content: "текст комментария", articleId: 1, createdAt: new Date(), updatedAt: new Date()},
-      {content: "текст комментария", articleId: 1, createdAt: new Date(), updatedAt: new Date()},
-      {content: "текст комментария", articleId: 2, createdAt: new Date(), updatedAt: new Date()}
+      {content: "Как круто!", articleId: 1, createdAt: new Date(), updatedAt: new Date()},
+      {content: "Согласен", articleId: 1, createdAt: new Date(), updatedAt: new Date()},
+      {content: "Супер!", articleId: 2, createdAt: new Date(), updatedAt: new Date()}
+      {content: "всё так!", articleId: 2, createdAt: new Date(), updatedAt: new Date()}
+      {content: "Может быть", articleId: 3, createdAt: new Date(), updatedAt: new Date()}
     ])
   },
 
