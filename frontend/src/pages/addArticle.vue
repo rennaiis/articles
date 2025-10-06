c0075214a478af60d00db9d34cb7eb43d000a623
a.isaeva <201250056+rennaiis@users.noreply.github.com>
2025-10-06 14:59:16 +0300

    Установка правил (текст и заголовок не могут быть пустыми)

--- frontend/src/pages/addArticle.vue
+++ frontend/src/pages/addArticle.vue
@@ -3,17 +3,28 @@
     <v-form fast-fail @submit.prevent>
       <v-text-field
         v-model="firstName"
+        :rules="titleRules"
         label="Заголовок статьи"
       ></v-text-field>
 
       <v-textarea
         v-model="lastName"
+        :rules="contentRules"
         label="текст статьи"
       ></v-textarea>
       <v-btn class="mt-2" type="submit" block>Создать</v-btn>
     </v-form>
   </v-sheet>
 </template>
-<script>
+
+<script setup>
+    import { onMounted, ref } from 'vue'
+    import axios from 'axios'
+    const titleRules =[
+        c => !! c ||'название не может быть пустым'
+    ]
+    const contentRules =[
+        c => !! c ||'текст не может быть пустым'
+    ]
 
 </script>
