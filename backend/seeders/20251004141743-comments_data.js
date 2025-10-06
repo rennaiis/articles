559806cd5538cd8ffa71a4290365a0e8deafb91d
rennaiis <isaeva.nastya04@gmail.com>
2025-10-07 01:29:10 +0300

    Исправлен readme и ошибки

--- backend/seeders/20251004141743-comments_data.js
+++ backend/seeders/20251004141743-comments_data.js
@@ -6,9 +6,9 @@ module.exports = {
     await queryInterface.bulkInsert("Comments", [
       {content: "Как круто!", articleId: 1, createdAt: new Date(), updatedAt: new Date()},
       {content: "Согласен", articleId: 1, createdAt: new Date(), updatedAt: new Date()},
-      {content: "Супер!", articleId: 2, createdAt: new Date(), updatedAt: new Date()}
-      {content: "всё так!", articleId: 2, createdAt: new Date(), updatedAt: new Date()}
-      {content: "Может быть", articleId: 3, createdAt: new Date(), updatedAt: new Date()}
+      {content: "Супер!", articleId: 2, createdAt: new Date(), updatedAt: new Date()},
+      {content: "всё так!", articleId: 2, createdAt: new Date(), updatedAt: new Date()},
+      {content: "Может быть", articleId: 3, createdAt: new Date(), updatedAt: new Date()},
     ])
   },
 
